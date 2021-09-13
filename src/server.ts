bafd5a44d03a341f006c6015e380de66b375ae03
Martin <majok5040@gmail.com>
2021-09-13 09:11:24 -0500

    project creation
    dependencies installation, server and environment variables

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import compression from 'compression';
+import { createServer } from 'http';
+
+const app = express();
+
+app.use(cors());
+
+app.use(compression());
+
+app.get('/', (_, res) => {
+    res.send('API');
+});
+
+const httpServer = createServer(app);
+
+httpServer.listen(
+    {
+        port: 3000
+    },
+    () => console.log('http://localhost:3000 API')
+);
